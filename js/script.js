d4f85587c0ef911845cdb93fc20a775aecc4d637
Md-yeasin <m.yeasin.me@gmail.com>
2022-08-31 03:11:06 +0600

    Adding last modified

--- js/script.js
+++ js/script.js
@@ -1,187 +1,51 @@
 function check(){
-    let c = 0;
-    var q1 = document.quize.options1.value;
-    var q2 = document.quize.options2.value;
-    var q3 = document.quize.options3.value;
-    var q4 = document.quize.options4.value;
-    var q5 = document.quize.options5.value;
-    var q6 = document.quize.options6.value;
-    var q7 = document.quize.options7.value;
-    var q8 = document.quize.options8.value;
-    var q9 = document.quize.options9.value;
-    var q10 = document.quize.options10.value;
-
-    var otcontentbody = document.getElementById('otcontentbody');
+    var c = 0;
+    var questionsarea = document.getElementsByClassName('questionarea');
+    var option = [document.quize.options1.value, document.quize.options2.value, document.quize.options3.value, document.quize.options4.value, document.quize.options5.value, document.quize.options6.value, document.quize.options7.value, document.quize.options8.value, document.quize.options9.value, document.quize.options10.value];
+    var dynamicscore = document.getElementById('dynamicscore');
     var result = document.getElementById('result');
-    var urscore = document.getElementById('urscore');
-   
+    var otcontentbody = document.getElementById('otcontentbody');
+    var dprinword = document.getElementById('dprinword');
+    var resultdetailsheading = document.getElementById('resultdetailsheading');
+    var resultdetaildescription = document.getElementById('resultdetaildescription');
 
-   
-    if (q1==0){
-        c+=0;
+    for(let i=0; i<questionsarea.length; i++){
+       
+        if (option[i]==0){
+            c+=0;
+        }
+        else if(option[i]==1){
+            c+=1;
+        }
+        else if(option[i]==2){
+            c+=2;
+        }
+        else {
+            c+=3;
+        }
     }
-    else if(q1==1){
-        c+=1;
-    }
-    else if(q1==2){
-        c+=2;
-    }
-    else {
-        c+=3;
-    }
-
-    if (q2==0){
-        c+=0;
-    }
-    else if(q2==1){
-        c+=1;
-    }
-    else if(q2==2){
-        c+=2;
-    }
-    else {
-        c+=3;
-    }
-
-    if (q3==0){
-        c+=0;
-    }
-    else if(q3==1){
-        c+=1;
-    }
-    else if(q3==2){
-        c+=2;
-    }
-    else {
-        c+=3;
-    }
-
-    if (q4==0){
-        c+=0;
-    }
-    else if(q4==1){
-        c+=1;
-    }
-    else if(q4==2){
-        c+=2;
-    }
-    else {
-        c+=3;
-    }
-
-    if (q5==0){
-        c+=0;
-    }
-    else if(q5==1){
-        c+=1;
-    }
-    else if(q5==2){
-        c+=2;
-    }
-    else {
-        c+=3;
-    }
-
-    if (q6==0){
-        c+=0;
-    }
-    else if(q6==1){
-        c+=1;
-    }
-    else if(q6==2){
-        c+=2;
-    }
-    else {
-        c+=3;
-    }
-
-    if (q7==0){
-        c+=0;
-    }
-    else if(q7==1){
-        c+=1;
-    }
-    else if(q7==2){
-        c+=2;
-    }
-    else {
-        c+=3;
-    }
-
-    if (q8==0){
-        c+=0;
-    }
-    else if(q8==1){
-        c+=1;
-    }
-    else if(q8==2){
-        c+=2;
-    }
-    else {
-        c+=3;
-    }
-
-    if (q9==0){
-        c+=0;
-    }
-    else if(q9==1){
-        c+=1;
-    }
-    else if(q9==2){
-        c+=2;
-    }
-    else {
-        c+=3;
-    }
-
-    if (q10==0){
-        c+=0;
-    }
-    else if(q10==1){
-        c+=1;
-    }
-    else if(q10==2){
-        c+=2;
-    }
-    else {
-        c+=3;
-    }
-    var total= c;
-    // window.alert(c);
-    // document.write(c);
-    // document.getElementById('urscore').innerHTML= total;
-    otcontentbody.style.display="none";
+ 
+    dynamicscore.textContent='Your result is ${c}.';
     result.style.display="block";
-    // document.write((c*100)/30);
+    otcontentbody.style.display="none";
+
+    
+    // if((c*100)/30=0){
+    //     dprinword.textContent='Minimul Depression';
+    //     resultdetailsheading = 'Your results indicate that you have none, or very few signs of depression.';
+    //     resultdetaildescription = 'These results are not meant to be a diagnosis. You can meet with a doctor or therapist to get a diagnosis and/or access therapy or medications. Sharing these results with someone you trust can be a great place to start.';
+    // }
+    // else if(c*100)/30=<50){
+    //     dprinword.textContent='You are in Depression';
+    //     resultdetailsheading = 'Your results indicate that you have none, or very few signs of depression.';
+    //     resultdetaildescription = 'These results are not meant to be a diagnosis. You can meet with a doctor or therapist to get a diagnosis and/or access therapy or medications. Sharing these results with someone you trust can be a great place to start.';
+    // }
+    // else{
+    //     dprinword.textContent='You are in most Depression';
+    //     resultdetailsheading = 'Your results indicate that you may be experiencing signs of moderately severe depression.';
+    //     resultdetaildescription = 'These results are not meant to be a diagnosis. You can meet with a doctor or therapist to get a diagnosis and/or access therapy or medications. Sharing these results with someone you trust can be a great place to start.';
+    // }
 }
 
 
-// .submitbtn.onclick = ()=>{
-//         const scoreText = result.querySelector(".urscore");
-//         const threesentence = result.querySelector(".dprinword");
-//         const heading = result.querySelector(".resultdetailsheading");
-//         const depression = result.querySelector(".resultdetaildescription");
-//         if (c = 0){ 
-           
-//             let scoreTag = c;
-//             scoreText.innerHTML = scoreTag;  
-//             threesentence.innerHTML = "Minimal Depression"; 
-//             heading.innerHTML = "Your results indicate that you have none, or very few signs of depression."; 
-//             depression.innerHTML = "These results are not meant to be a diagnosis. You can meet with a doctor or therapist to get a diagnosis and/or access therapy or medications. Sharing these results with someone you trust can be a great place to start.";  
 
-//         else if(c <= 50){ // if user scored more than 1
-//             let scoreTag = c;
-//             scoreText.innerHTML = scoreTag;  
-//             threesentence.innerHTML = "Minimal Depression"; 
-//             heading.innerHTML = "Your results indicate that you have none, or very few signs of depression."; 
-//             depression.innerHTML = "These results are not meant to be a diagnosis. You can meet with a doctor or therapist to get a diagnosis and/or access therapy or medications. Sharing these results with someone you trust can be a great place to start."; 
-//         }
-//         else{ // if user scored less than 1
-//             let scoreTag = c;
-//             scoreText.innerHTML = scoreTag;  
-//             threesentence.innerHTML = "Minimal Depression"; 
-//             heading.innerHTML = "Your results indicate that you have none, or very few signs of depression."; 
-//             depression.innerHTML = "These results are not meant to be a diagnosis. You can meet with a doctor or therapist to get a diagnosis and/or access therapy or medications. Sharing these results with someone you trust can be a great place to start."; 
-//         }
-   
-// }
